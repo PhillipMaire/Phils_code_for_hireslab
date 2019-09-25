8b763fb452921125756cbff65350c2a65b4d08ed
Jinho Kim <jkimkj@gmail.com>
2019-09-24 20:23:00 -0700

    fixed a few bugs that ran the wrong cells

--- GLM/shinyCode/poissonGLM.m
+++ GLM/shinyCode/poissonGLM.m
@@ -1,6 +1,6 @@
 %{
-
-winopen(saveForTmp1Models)
+    
+winopen(saveForTmpModels)
 
 
 
@@ -8,22 +8,42 @@ dateString = datestr(now,'yymmdd_HHMM');
 
 
 
+
+
+
+minNumWorkers = 10;
+
+numCores = 16;
 leftToRun = 1;
 
 counter1 = 0;
 triggerParForEnd = 1;
 while true
 counter1 = counter1+1;
-    delete(gcp('nocreate'))
-    try
-        poissonGLM
-    catch
-    end    
-    if sum(leftToRun(:)) == 0;
-        triggerParForEnd = 0 ;
+if isempty(gcp('nocreate'))
+    parpool(numCores)
+else
+    p = gcp;
+    if p.NumWorkers <= minNumWorkers
+        delete(gcp('nocreate'))
+        parpool(numCores)
     end
-    pBullet = Pushbullet('o.LLzZkkdcd6WN8MG5HyQjFDsCCSmRBAzw')
-    pBullet.pushNote([],'model!',['failed ',num2str(counter1) , ' times'])
+end
+try
+    poissonGLM
+catch ME
+    if strcmp(ME.message, 'too few cores are running')
+        disp('yay my parfor loop catcher is working!')
+        
+    else
+        keyboard
+    end
+end
+if sum(leftToRun(:)) == 0;
+    triggerParForEnd = 0 ;
+end
+pBullet = Pushbullet('o.LLzZkkdcd6WN8MG5HyQjFDsCCSmRBAzw')
+pBullet.pushNote([],'model!',['failed ',num2str(counter1) , ' times'])
 end
 
 
@@ -45,18 +65,17 @@ mkdir(userDir);
 %%
 modelRunName = 'buildDM_001 simple model 2 basisFunctionForTouches_entireTrial_5msBIN';
 
-minNumWorkers = 2;
 numTimesToRunModel = 10;
 % % % % load('Y:\tmpForTransfer\PHILS_GLM_CODE\U_181027_2358.mat')
 %only run these cells
 % theseCells = 1:length(U);
 load('Y:\tmpForTransfer\PHILS_GLM_CODE\cells2run.mat')
 theseCells = cells2run;
-theseCells = theseCells(1:ceil(length(theseCells)./1.5))
+theseCells = theseCells(ceil(length(theseCells)./1.5):length(theseCells))
+theseCells = 1
 
-
-saveForTmp1Models = 'C:\Users\maire\Dropbox\HIRES_LAB\PHIL\Data\GLMs\tmpSavesForGLMmodels';
-saveForTmp1Models = [saveForTmp1Models, '_', dateString];
+saveForTmpModels = 'C:\Users\shires\Documents\PHILS_GLM_SAVES\tmpSaveForModels';
+saveForTmpModels = [saveForTmpModels, '_', dateString];
 
 [trialTypes, trialTypeNames] = UtrialType(U);
 load('Y:\tmpForTransfer\PHILS_GLM_CODE\OnsetsALL_CELLS_190917.mat')
@@ -72,8 +91,8 @@ try
     U = msAndRoundUarray(U, 'ms');
 catch
 end
-mkdir(saveForTmp1Models)
-cd(saveForTmp1Models)
+mkdir(saveForTmpModels)
+cd(saveForTmpModels)
 
 modsRanAlready = dir('*_modRunNum_*');
 numModelsRunTotal = length(modsRanAlready);
@@ -91,23 +110,26 @@ for k = 1:size(modsRanAlready, 1)
     
 end
 
-[dim1, dim2] = find(leftToRun);
-theseCells = intersect(theseCells, unique(dim1));
+[dim1, ~] = find(leftToRun);
+theseCells = intersect(theseCells, theseCells(unique(dim1)));
 
 if sum(leftToRun(:)) == 0;
     triggerParForEnd = 0 ;
 end
 for cellStepTMP = 1:length(theseCells)
     cellStep = theseCells(cellStepTMP);
-    ModelItersToRun = dim2(find(dim1==cellStep));
-    if isnan(cellsThanHaveRun(cellStepTMP))
-        C = U{cellStep};
+    ModelItersToRun = find(leftToRun(cellStepTMP, :));
+    %     if isnan(cellsThanHaveRun(cellStepTMP))
+    C = U{cellStep};
+    
+    Y = squeeze(C.R_ntk);
+    tmp1 = nanmean(Y(:)).*1000;
+    if tmp1>1
         
-        Y = squeeze(C.R_ntk);
-        tmp1 = nanmean(Y(:)).*1000;
-        if tmp1>1
-            
-            tic
+        tic
+        if ~isempty(dir(['*tmp_cell_', num2str(cellStep), '_DMsettings*']))
+            load(['tmp_cell_', num2str(cellStep), '_DMsettings.mat'])
+        else
             [DM, DMsettings] = buildDM_001(U, OnsetsALL_CELLS, trialTypes, cellStep);
             %% randomly select data to model
             perc2model = .7;
@@ -125,39 +147,39 @@ for cellStepTMP = 1:length(theseCells)
             DMsettings.label = DM.label;
             DMsettings.numInEachLabel = DM.numInEachLabel;
             save(['tmp_cell_', num2str(cellStep), '_DMsettings'], 'DMsettings')
-            
-            for iterateModel = ModelItersToRun(:)'
-                
-                [DM2] = sampleDMandY(perc2model, limitModelTrialNumsTo,msRangeToModel, C, DM);
-                
-                %
-                glmNetOptions = struct;
-                glmNetOptions.alpha= .95;
-                % glmnetSet
-                
-                
-                DM2.model = cvglmnet(DM2.X2model, DM2.Y2model, 'poisson', glmNetOptions);
-                
-                DM2.modLoc = find(DM2.model.lambda ==DM2.model.lambda_1se);
-                % to save just the important parts
-                keepFields = {'model' 'tomodel' 'toTest' 'modLoc' 'perc2model'};%remove allbut these fields
-                DM2 = rmfield(DM2,setdiff(fieldnames(DM2), keepFields));
-                parForSaveGLM(['tmp_cell_', num2str(cellStep), '_modRunNum_', num2str(iterateModel)], DM2)
-                
-                
-                parallelHackForGLMnet(minNumWorkers, userDir,numModelsRunTotal);
-                
-            end
-            
-            
-            timeItTook = toc
-            pBullet = Pushbullet('o.LLzZkkdcd6WN8MG5HyQjFDsCCSmRBAzw')
-            pBullet.pushNote([],'model',['finished cell ', num2str(cellStep), ' in ', num2str(timeItTook), ' sec'])
-            cellsThanHaveRun(cellStepTMP) = 1;
         end
-    else
-        pause(5)
+        parfor iterateModelTMP = 1:length(ModelItersToRun)
+            iterateModel = ModelItersToRun(iterateModelTMP);
+            [DM2] = sampleDMandY(perc2model, limitModelTrialNumsTo,msRangeToModel, C, DMsettings);
+            
+            %
+            glmNetOptions = struct;
+            glmNetOptions.alpha= .95;
+            % glmnetSet
+            
+            
+            DM2.model = cvglmnet(DM2.X2model, DM2.Y2model, 'poisson', glmNetOptions);
+            
+            DM2.modLoc = find(DM2.model.lambda ==DM2.model.lambda_1se);
+            % to save just the important parts
+            keepFields = {'model' 'tomodel' 'toTest' 'modLoc' 'perc2model'};%remove allbut these fields
+            DM2 = rmfield(DM2,setdiff(fieldnames(DM2), keepFields));
+            parForSaveGLM(['tmp_cell_', num2str(cellStep), '_modRunNum_', num2str(iterateModel)], DM2)
+            
+            
+            parallelHackForGLMnet(minNumWorkers, userDir,numModelsRunTotal);
+            
+        end
+        
+        
+        timeItTook = toc
+        pBullet = Pushbullet('o.LLzZkkdcd6WN8MG5HyQjFDsCCSmRBAzw')
+        pBullet.pushNote([],'model',['finished cell ', num2str(cellStep), ' in ', num2str(timeItTook), ' sec'])
+        %             cellsThanHaveRun(cellStepTMP) = 1;
     end
+    %     else
+    %         pause(5)
+    %     end
 end
 
 
